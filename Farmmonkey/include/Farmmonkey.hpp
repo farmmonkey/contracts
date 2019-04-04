04eefca4bc050394e76580afbd7db7e31c9d0949
shawnrmoss <me@shawnrmoss.ca>
2019-04-03 21:34:56 -0600

    Added person table

--- Farmmonkey/include/Farmmonkey.hpp
+++ Farmmonkey/include/Farmmonkey.hpp
@@ -5,7 +5,32 @@ CONTRACT Farmmonkey : public contract {
    public:
       using contract::contract;
 
+      Farmmonkey(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
+
       ACTION hi( name nm );
 
       using hi_action = action_wrapper<"hi"_n, &Farmmonkey::hi>;
+      
+      ACTION upsert(name user, std::string first_name, std::string last_name, std::string street, std::string city, std::string state);
+
+      using upsert_action = action_wrapper<"upsert"_n, &Farmmonkey::upsert>;
+      
+      ACTION erase(name user);
+
+      using erase_action = action_wrapper<"erase"_n, &Farmmonkey::erase>;
+      
+
+   private:
+      struct [[eosio::table]] person {
+         name key;
+         std::string first_name;
+         std::string last_name;
+         std::string street;
+         std::string city;
+         std::string state;
+
+         uint64_t primary_key() const { return key.value;}
+      };
+
+      typedef eosio::multi_index<"people"_n, person> address_index;
 };
--- Farmmonkey/src/Farmmonkey.cpp
+++ Farmmonkey/src/Farmmonkey.cpp
@@ -1,5 +1,43 @@
 #include <Farmmonkey.hpp>
+
 ACTION Farmmonkey::hi( name nm ) {
    /* fill in action body */
    print_f("Name : %\n",nm);
+}
+
+ACTION Farmmonkey::upsert(name user, std::string first_name, std::string last_name, std::string street, std::string city, std::string state) {
+   require_auth( user );
+   address_index addresses( get_self(), get_first_receiver().value );
+   auto iterator = addresses.find(user.value);
+   if( iterator == addresses.end() )
+   {
+      addresses.emplace(user, [&]( auto& row ) {
+      row.key = user;
+      row.first_name = first_name;
+      row.last_name = last_name;
+      row.street = street;
+      row.city = city;
+      row.state = state;
+      });
+   }
+   else {
+      std::string changes;
+      addresses.modify(iterator, user, [&]( auto& row ) {
+      row.key = user;
+      row.first_name = first_name;
+      row.last_name = last_name;
+      row.street = street;
+      row.city = city;
+      row.state = state;
+      });
+   }
+}
+
+ACTION Farmmonkey::erase(name user) {
+   require_auth(user);
+   address_index addresses( get_self(), get_first_receiver().value);
+
+   auto iterator = addresses.find(user.value);
+   check(iterator != addresses.end(), "Record does not exist");
+   addresses.erase(iterator);
 }
